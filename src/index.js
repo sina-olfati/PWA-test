20d0e14a89ac883bda0b7c9baeda2fb432305366
sina <sinaolfati5@gmail.com>
2022-06-11 18:29:22 +0430

    completed it || fix all the errors in lighthouse

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("../sw.js").then(registration => {
+    navigator.serviceWorker.register("/sw.js").then(registration => {
         console.log("ServiceWorker registration successful with scope: ", registration);
     }).catch(err => {
         console.log("ServiceWorker registration failed: ", err);
--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
 self.addEventListener("install", e => {
-    console.log("Installed!")
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./", "./src/master.css", "./images/pp1.png"])
+        }
+    ))
+})
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request)
+        })
+    )
 })
