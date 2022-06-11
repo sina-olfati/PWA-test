da53367e1d0da5f028471ffa459b01ab050a880f
sina <sinaolfati5@gmail.com>
2022-06-11 17:36:45 +0430

    making the first lines of sw.js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/sw.js").then(registration => {
-        console.log("ServiceWorker registration successful with scope: ", registration.scope);
+    navigator.serviceWorker.register("../sw.js").then(registration => {
+        console.log("ServiceWorker registration successful with scope: ", registration);
     }).catch(err => {
         console.log("ServiceWorker registration failed: ", err);
     })
